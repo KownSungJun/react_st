7658681f3612e878cceeb4b855c3f56fa3b13ca2
권성준 <58690800+KownSungJun@users.noreply.github.com>
2025-12-21 14:58:43 +0000

    기본 컴포넌트 구성

---
+++ todo-app/src/components/TodoTemplate.js
@@ -0,0 +1,15 @@
+import './TodoTemplate.scss'
+/**
+ * 화면을 가운데에 정렬시켜 주며, 앱 타이틀(일정 관리)을 보여준다.
+ * children으로 내부 JSX를 props로 받아 와서 렌더링 해준다.
+ */
+const TodoTemplate = ({children}) => {
+    return (
+        <div className='TodoTemplate'>
+            <div className='app-title'>일정 관리</div>
+            <div className='content'>{children}</div>
+        </div>
+    )
+}
+
+export default TodoTemplate;
