cbb7df9c0e32d7b441334f094457b799907f0d77
권성준 <58690800+KownSungJun@users.noreply.github.com>
2025-12-22 07:27:26 +0000

    리스트 item 추가 + 디자인 추가

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,29 @@
+import { useState } from "react"
+import TodoInsert from "./components/TodoInsert"
 import TodoTemplate from "./components/TodoTemplate"
-
+import TodoList from "./components/TodoList"
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '운동하기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '리엑트 기초 공부',
+      checked: false,
+    },
+    {
+      id: 3,
+      text: '아이디어 브레인 스토밍하기',
+      checked: false,
+    },
+  ])
+  return <TodoTemplate>
+    <TodoInsert />
+    <TodoList todos={todos}/>
+  </TodoTemplate>
 }
 
 export default App
