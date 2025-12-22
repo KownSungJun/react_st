be4481dfe4e0ef2a92293f039bec466e5e65071f
권성준 <58690800+KownSungJun@users.noreply.github.com>
2025-12-22 14:46:56 +0000

    리엑트 hook, router 공부

---
+++ hooks-tutorial/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Counter from './Counter'
+import './App.css'
+import Info from './Info'
+function App() {
+
+  return (
+    <>
+      <Counter />
+      <Info />
+    </>
+  )
+}
+
+export default App
---
+++ hooks-tutorial/src/Info.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+//useState 예제
+// const Info = () => {
+//     const [name, setName] = useState('')
+//     const [nickname, setNickname] = useState('')
+
+//     const onChangeName = e => {
+//         setName(e.target.value)
+//     }
+
+//     const onChangeNickName = e => {
+//         setNickname(e.target.value)
+//     }
+
+//     return <>
+//         <div>
+//             <input value={name} onChange={onChangeName} />
+//             <input value={nickname} onChange={onChangeNickName} />
+//         </div>
+//         <div>
+//             <div>
+//                 <b>이름:</b> {name}
+//             </div>
+//             <div>
+//                 <b>닉네임:</b> {nickname}
+//             </div>
+//         </div>
+//     </>
+// }
+
+//useEffect 예제
+const Info = () => {
+    const [name, setName] = useState('')
+    const [nickname, setNickname] = useState('')
+    // useEffect(() => {
+    //     console.log('렌더링이 완료되었습니다!')
+    //     console.log({
+    //         name, nickname
+    //     })
+    // })
+    useEffect(() => {
+        console.log('마운트될 때만 실행됩니다.')
+    })
+    const onChangeName = e => {
+        setName(e.target.value)
+    }
+
+    const onChangeNickName = e => {
+        setNickname(e.target.value)
+    }
+
+    return <>
+         <div>
+             <input value={name} onChange={onChangeName} />
+             <input value={nickname} onChange={onChangeNickName} />
+         </div>
+         <div>
+             <div>
+                 <b>이름:</b> {name}
+             </div>
+             <div>
+                 <b>닉네임:</b> {nickname}
+             </div>
+         </div>
+     </>
+}
+
+export default Info;
